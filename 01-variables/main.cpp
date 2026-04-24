52cb67fed3effd9280f8b40050f86eef8b270b19
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2026-04-24 11:22:27 +0100

    variables

---
+++ 01-variables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // Variable declarations and initializations
+    int age; // Interger variable declaration
+    age = 25; // Initizliation
+    float temperature = 38.8f; // Float variable (note the 'f' suffix for float literal)
+    bool isOn = true; // Boolean variable
+
+    std::cout << age << ", " << temperature << ", " << isOn << std::endl;
+
+    return 0;  // Program executed successfully
+}
