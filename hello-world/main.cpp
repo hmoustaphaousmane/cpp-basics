7ba7e49b2cbfcd077d38c11f82e55acba7314440
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2026-04-24 10:52:26 +0100

    hello world

---
+++ hello-world/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+/*
+#include <iostream>: Une directive de préprocesseur qui inclut la bibliothèque d'entrée/sortie standard, permettant la sortie de la console.
+int main(): Fonction de point d'entrée où commence l'exécution du programme.
+std::cout << ...: Opérateur de flux de sortie utilisé pour imprimer la chaîne sur le terminal.
+return 0;: Indique que le programme s'est terminé avec succès.
+*/
